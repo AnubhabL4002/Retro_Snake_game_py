93eec90dc1572ae9980ea8aac1c67a14665aad4c
Anubhab Lahiri <158812807+AnubhabL4002@users.noreply.github.com>
2025-01-24 16:51:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ while game_is_on:
         scoreboard.update_score()
 
     # Detect collision with wall.
-    if snake.head.xcor()>280 or snake.head.xcor() < -300 or snake.head.ycor()>300 or snake.head.ycor()<-280:
+    if snake.head.xcor()>280 or snake.head.xcor() < -280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
         gameover.game_over_display()
         game_is_on = False
 
